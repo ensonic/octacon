bdbcd26da263e8702eecfec1d6c9868d347e4322
Stefan Sauer <st.kost@gmail.com>
2025-03-26 15:47:29 +0100

    'Repair' printf.
    Yes, there are limitations, but ...

--- src/debug.cpp
+++ src/debug.cpp
@@ -22,8 +22,16 @@ void Debug::println(const char* s) {
 void Debug::printf(const char* fmt, ...) {
     if (!serial) return;
 
-    va_list args;
-    va_start(args, fmt);
-    serial->printf(fmt, args);
-    va_end(args);
+    // meh there is no vprintf, so copy the from Print.cpp (and yes,
+    // there is a todo about the author wondering why the need to copy it)
+    va_list arg;
+    va_start(arg, fmt);
+    char temp[80];
+    size_t len = vsnprintf(temp, sizeof(temp), fmt, arg);
+    va_end(arg);
+    if (len > sizeof(temp) - 1) {
+        // lets simply truncate, this is for logging ...
+        temp[sizeof(temp) - 1] = '\0';
+    }
+    serial->write((const uint8_t*) temp, len);
 }
